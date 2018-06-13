b74f729d4fe572c619f4277419f6773bba8c7b1f
林少聪 < @PC-20180515PRDG>
2018-06-13 19:25:36 +0800

    解决冲突

--- src/main/java/com/anbang/qipai/admin/remote/service/QipaiMembersService.java
+++ src/main/java/com/anbang/qipai/admin/remote/service/QipaiMembersService.java
@@ -1,9 +1,11 @@
 package com.anbang.qipai.admin.remote.service;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.anbang.qipai.admin.plan.domain.ClubCard;
 import com.anbang.qipai.admin.remote.vo.CommonRemoteVO;
 
 /**
@@ -18,12 +20,27 @@ public interface QipaiMembersService {
 	 * 这里定义了类似于SpringMVC用法的方法，就可以进行RESTful的调用了
 	 **/
 	@RequestMapping(value = "/conf/commonuser")
-	public CommonRemoteVO commonuser(@RequestParam(value = "signGoldNumber")Integer signGoldNumber,@RequestParam(value = "goldForNewNember")Integer goldForNewNember,@RequestParam(value = "shareIntegralNumber")Integer
-			shareIntegralNumber,@RequestParam(value = "shareGoldNumber")Integer shareGoldNumber,@RequestParam(value = "inviteIntegralNumber")Integer inviteIntegralNumber,@RequestParam(value = "planGrowIntegralSpeed")float planGrowIntegralSpeed);
-	
-	@RequestMapping(value = "/conf/vipuser")
-	public CommonRemoteVO  vipuser(@RequestParam(value = "signGoldNumber")Integer signGoldNumber,@RequestParam(value = "shareIntegralNumber")Integer
-			shareIntegralNumber,@RequestParam(value = "shareGoldNumber")Integer shareGoldNumber,@RequestParam(value = "inviteIntegralNumber")Integer
-			inviteIntegralNumber,@RequestParam(value = "vipGrowIntegralSpeed")float vipGrowIntegralSpeed,@RequestParam(value = "vipGrowGradeSpeed")float vipGrowGradeSpeed);
+	public CommonRemoteVO commonuser(@RequestParam(value = "signGoldNumber") Integer signGoldNumber,
+			@RequestParam(value = "goldForNewNember") Integer goldForNewNember,
+			@RequestParam(value = "shareIntegralNumber") Integer shareIntegralNumber,
+			@RequestParam(value = "shareGoldNumber") Integer shareGoldNumber,
+			@RequestParam(value = "inviteIntegralNumber") Integer inviteIntegralNumber,
+			@RequestParam(value = "planGrowIntegralSpeed") float planGrowIntegralSpeed);
 
+	@RequestMapping(value = "/conf/vipuser")
+	public CommonRemoteVO vipuser(@RequestParam(value = "signGoldNumber") Integer signGoldNumber,
+			@RequestParam(value = "shareIntegralNumber") Integer shareIntegralNumber,
+			@RequestParam(value = "shareGoldNumber") Integer shareGoldNumber,
+			@RequestParam(value = "inviteIntegralNumber") Integer inviteIntegralNumber,
+			@RequestParam(value = "vipGrowIntegralSpeed") float vipGrowIntegralSpeed,
+			@RequestParam(value = "vipGrowGradeSpeed") float vipGrowGradeSpeed);
+
+	@RequestMapping(value = "/clubcard/addclubcard")
+	public CommonRemoteVO clubcard_add(@RequestBody ClubCard clubCard);
+
+	@RequestMapping(value = "/clubcard/deleteclubcards")
+	public CommonRemoteVO clubcard_delete(@RequestBody String[] clubCardIds);
+
+	@RequestMapping(value = "/clubcard/updateclubcard")
+	public CommonRemoteVO clubcard_update(@RequestBody ClubCard clubCard);
 }
