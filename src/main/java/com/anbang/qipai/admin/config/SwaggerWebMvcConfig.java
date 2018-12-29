6ad6d5e4f59602f4242dc591b0b934e3668b8db0
wushuohan <923310233@qq.com>
2018-12-29 09:25:59 +0800

    提交到正式服

--- src/main/java/com/anbang/qipai/admin/config/SwaggerWebMvcConfig.java
+++ src/main/java/com/anbang/qipai/admin/config/SwaggerWebMvcConfig.java
@@ -1,23 +1,23 @@
-package com.anbang.qipai.admin.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-/**
- * @Author: 吴硕涵
- * @Date: 2018/12/11 1:41 PM
- * @Version 1.0
- */
-@Configuration
-public class SwaggerWebMvcConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-
-}
+//package com.anbang.qipai.admin.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+///**
+// * @Author: 吴硕涵
+// * @Date: 2018/12/11 1:41 PM
+// * @Version 1.0
+// */
+//@Configuration
+//public class SwaggerWebMvcConfig extends WebMvcConfigurerAdapter {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+//
+//}
