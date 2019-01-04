e85fa59a756926ab5b88f7a84310aeacdbb75374
ethan0353 <yinziyu7@gmail.com>
2019-01-04 14:13:35 +0800

    fix exclude

--- src/main/java/com/anbang/qipai/admin/web/WebMvcConfig.java
+++ src/main/java/com/anbang/qipai/admin/web/WebMvcConfig.java
@@ -58,7 +58,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(loginInterceptor()).addPathPatterns("/**").excludePathPatterns("/login/**");
+		registry.addInterceptor(loginInterceptor()).addPathPatterns("/**").excludePathPatterns("/login/**","/game/querylastgameversion");
 		registry.addInterceptor(permissionInterceptor()).addPathPatterns("/**").excludePathPatterns("/login/**","/game/querylastgameversion");
 	}
 
