74c09ecb1990838eef6133728b422e08bc0fb018
林少聪 < @PC-20180515PRDG>
2019-01-04 16:35:37 +0800

    bugfix

--- src/main/java/com/anbang/qipai/admin/web/vo/agentsvo/RecordSummaryTexts.java
+++ src/main/java/com/anbang/qipai/admin/web/vo/agentsvo/RecordSummaryTexts.java
@@ -1,7 +1,7 @@
 package com.anbang.qipai.admin.web.vo.agentsvo;
 
 public enum RecordSummaryTexts {
-	新推广员, 邀请玩家, 管理员调整, 积分兑换会员卡, 微信购买会员卡, 微信购买玉石, 转赠下级, 上级转赠, 充值玩家VIP;
+	新推广员, 邀请玩家, 管理员调整, 积分兑换会员卡, 微信购买会员卡, 微信购买玉石, 转赠下级, 上级转赠, 充值玩家VIP, 充值玩家玉石;
 
 	public static String getSummaryText(String text) {
 		if (text == null) {
@@ -26,6 +26,8 @@ public enum RecordSummaryTexts {
 			return 上级转赠.name();
 		case "recharge member":
 			return 充值玩家VIP.name();
+		case "recharge member gold":
+			return 充值玩家玉石.name();
 		default:
 			return text;
 		}
