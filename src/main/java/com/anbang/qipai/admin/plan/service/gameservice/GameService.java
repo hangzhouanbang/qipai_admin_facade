15ab183e8b8788f905212f1a26d73e979c23d51a
ethan0353 <yinziyu7@gmail.com>
2019-01-08 17:29:47 +0800

    add roomquery

--- src/main/java/com/anbang/qipai/admin/plan/service/gameservice/GameService.java
+++ src/main/java/com/anbang/qipai/admin/plan/service/gameservice/GameService.java
@@ -9,8 +9,12 @@ import java.util.stream.Collectors;
 import com.anbang.qipai.admin.plan.bean.games.*;
 import com.anbang.qipai.admin.plan.bean.historicalresult.GameHistoricalJuResult;
 import com.anbang.qipai.admin.plan.bean.historicalresult.GameHistoricalPanResult;
+import com.anbang.qipai.admin.plan.bean.historicalresult.GameHistoricalPanResultExpand;
 import com.anbang.qipai.admin.plan.dao.gamedao.*;
+import com.anbang.qipai.admin.remote.service.QipaiGameRemoteService;
+import com.anbang.qipai.admin.remote.vo.CommonRemoteVO;
 import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.messaging.support.MessageBuilder;
@@ -48,6 +52,9 @@ public class GameService {
 
 	@Autowired
 	private GameHistoricalPanResultDao gameHistoricalPanResultDao;
+
+	@Autowired
+	private QipaiGameRemoteService qipaiGameRomoteService;
 
 	public void onlineGameServer(GameServer gameServer) {
 		gameServerDao.save(gameServer);
@@ -175,9 +182,26 @@ public class GameService {
 	public Map queryRoomDetail (String gameId) {
 		GameHistoricalJuResult juResult = gameHistoricalJuResultDao.findJuResultBygameId(gameId);
 		List<GameHistoricalPanResult> panResults = gameHistoricalPanResultDao.findPanResultByGameId(gameId);
+		List<GameHistoricalPanResultExpand> result = new ArrayList<>();
+		for (GameHistoricalPanResult list : panResults) {
+			GameHistoricalPanResultExpand expand = new GameHistoricalPanResultExpand();
+			BeanUtils.copyProperties(list, expand);
+			// 获取返回码
+			CommonRemoteVO commonRemoteVO = qipaiGameRomoteService.queryBackcode(list.getGame(), list.getGameId(), list.getNo());
+			if (commonRemoteVO.getData() == null) {
+				expand.setCodeStatus(0);
+			} else {
+				expand.setCodeStatus(1);
+				expand.setCode(String.valueOf(commonRemoteVO.getData()));
+			}
+			result.add(expand);
+		}
+
 		Map map = new HashMap();
-		map.put("totalScore", juResult.getPlayerResultList());
-		map.put("list", panResults);
+		if (juResult != null) {
+			map.put("totalScore", juResult.getPlayerResultList());
+		}
+		map.put("list", result);
 		return map;
 	}
 
