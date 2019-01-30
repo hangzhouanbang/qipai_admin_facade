f6a4fd913dbdc3e81b22150e002e283c27235bc0
吴硕涵 <923310233@qq.com>
2019-01-30 17:09:53 +0800

    修改

--- src/main/java/com/anbang/qipai/admin/msg/receiver/memberreceiver/MembersMsgReceiver.java
+++ src/main/java/com/anbang/qipai/admin/msg/receiver/memberreceiver/MembersMsgReceiver.java
@@ -1,5 +1,8 @@
 package com.anbang.qipai.admin.msg.receiver.memberreceiver;
 
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -16,58 +19,61 @@ import com.google.gson.Gson;
 @EnableBinding(MembersSink.class)
 public class MembersMsgReceiver {
 
-	@Autowired
-	private MemberDboService memberService;
+    Logger logger = LoggerFactory.getLogger(MembersMsgReceiver.class);
+    @Autowired
+    private MemberDboService memberService;
 
-	@Autowired
-	private DetailedReportService reportService;
+    @Autowired
+    private DetailedReportService reportService;
 
-	private Gson gson = new Gson();
+    private Gson gson = new Gson();
 
-	@StreamListener(MembersSink.MEMBERS)
-	public void recordMember(CommonMO mo) {
-		String msg = mo.getMsg();
-		String json = gson.toJson(mo.getData());
-		MemberDbo member = gson.fromJson(json, MemberDbo.class);
-		if ("newMember".equals(msg)) {
-			memberService.addMember(member);
-			// 报表功能,统计新增用户和用户总量
-			// createTime转化为以天为精度的时间戳
-			Long createTime = TimeUtil.getTimeWithDayPrecision(member.getCreateTime());
-			// 得到用户总量
-			int totalUserCount = (int) memberService.countAmount();
-			// 组装detailedReport存入数据库,新增用户字段自增一
-			DetailedReport report = new DetailedReport(createTime);
-			report.setTotalUserCount(totalUserCount);
-			reportService.upsertAddUserData(report);
-		}
-		if ("update member phone".equals(msg)) {
-			memberService.updateMemberPhone(member);
-		}
-		if ("update member info".equals(msg)) {
-			memberService.updateMemberBaseInfo(member);
-		}
-		if ("update member vip".equals(msg)) {
-			memberService.updateMemberVip(member);
-		}
-		if ("memberOrder delive".equals(msg)) {
-			memberService.memberOrderDelive(member);
-		}
-		if ("recharge vip".equals(msg)) {
-			memberService.rechargeVip(member);
-		}
-		if ("update member realUser".equals(msg)) {
-			memberService.updateMemberRealUser(member);
-		}
-		if ("update member bindAgent".equals(msg)) {
-			memberService.updateMemberBindAgent(member.getId(), member.getAgentId(), member.isBindAgent());
-		}
-		if ("remove member bindAgent".equals(msg)) {
-			memberService.removeMemberBindAgent(member.getId());
-		}
-		if ("add member bindAgent".equals(msg)) {
-			memberService.updateMemberBindAgent(member.getId(), member.getAgentId(), member.isBindAgent());
-		}
-	}
+    @StreamListener(MembersSink.MEMBERS)
+    public void recordMember(CommonMO mo) {
+        logger.info("hahahah" + JSON.toJSONString(mo));
+        logger.info("bbbbb" + mo.toString());
+        String msg = mo.getMsg();
+        String json = gson.toJson(mo.getData());
+        MemberDbo member = gson.fromJson(json, MemberDbo.class);
+        if ("newMember".equals(msg)) {
+            memberService.addMember(member);
+            // 报表功能,统计新增用户和用户总量
+            // createTime转化为以天为精度的时间戳
+            Long createTime = TimeUtil.getTimeWithDayPrecision(member.getCreateTime());
+            // 得到用户总量
+            int totalUserCount = (int) memberService.countAmount();
+            // 组装detailedReport存入数据库,新增用户字段自增一
+            DetailedReport report = new DetailedReport(createTime);
+            report.setTotalUserCount(totalUserCount);
+            reportService.upsertAddUserData(report);
+        }
+        if ("update member phone".equals(msg)) {
+            memberService.updateMemberPhone(member);
+        }
+        if ("update member info".equals(msg)) {
+            memberService.updateMemberBaseInfo(member);
+        }
+        if ("update member vip".equals(msg)) {
+            memberService.updateMemberVip(member);
+        }
+        if ("memberOrder delive".equals(msg)) {
+            memberService.memberOrderDelive(member);
+        }
+        if ("recharge vip".equals(msg)) {
+            memberService.rechargeVip(member);
+        }
+        if ("update member realUser".equals(msg)) {
+            memberService.updateMemberRealUser(member);
+        }
+        if ("update member bindAgent".equals(msg)) {
+            memberService.updateMemberBindAgent(member.getId(), member.getAgentId(), member.isBindAgent());
+        }
+        if ("remove member bindAgent".equals(msg)) {
+            memberService.removeMemberBindAgent(member.getId());
+        }
+        if ("add member bindAgent".equals(msg)) {
+            memberService.updateMemberBindAgent(member.getId(), member.getAgentId(), member.isBindAgent());
+        }
+    }
 
 }
