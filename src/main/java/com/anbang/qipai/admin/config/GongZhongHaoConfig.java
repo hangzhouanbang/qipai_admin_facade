b3c1e9fea99050399ea8399877c645b4a916e1eb
林少聪 < @PC-20180515PRDG>
2019-02-11 14:04:06 +0800

    bugfix

---
+++ src/main/java/com/anbang/qipai/admin/config/GongZhongHaoConfig.java
@@ -0,0 +1,8 @@
+package com.anbang.qipai.admin.config;
+
+public class GongZhongHaoConfig {
+	public static String JSAPI_TICKET = "";
+	public static String ACCESSTOKEN = "";
+	public static final String APPID = " ";
+	public static final String APPSECRET = " ";
+}
