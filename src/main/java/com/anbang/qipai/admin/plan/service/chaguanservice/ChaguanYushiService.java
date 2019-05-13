bba0660900f23184af63fb00709ebb2e7fe61609
林少聪 < @PC-20180515PRDG>
2019-05-13 17:15:56 +0800

    bugfix

--- src/main/java/com/anbang/qipai/admin/plan/service/chaguanservice/ChaguanYushiService.java
+++ src/main/java/com/anbang/qipai/admin/plan/service/chaguanservice/ChaguanYushiService.java
@@ -1,13 +1,16 @@
 package com.anbang.qipai.admin.plan.service.chaguanservice;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.anbang.qipai.admin.plan.bean.chaguan.ChaguanYushiRecordDbo;
+import com.anbang.qipai.admin.plan.dao.agentsdao.AgentDboDao;
 import com.anbang.qipai.admin.plan.dao.chaguandao.ChaguanYushiRecordDboDao;
 import com.anbang.qipai.admin.web.vo.chaguanvo.ChaguanYushiRecordDboVO;
+import com.anbang.qipai.admin.web.vo.chaguanvo.ChaguanYushiRecordVO;
 import com.highto.framework.web.page.ListPage;
 
 @Service
@@ -16,10 +19,25 @@ public class ChaguanYushiService {
 	@Autowired
 	private ChaguanYushiRecordDboDao chaguanYushiRecordDboDao;
 
+	@Autowired
+	private AgentDboDao agentDboDao;
+
 	public ListPage findRecordByConditions(int page, int size, ChaguanYushiRecordDboVO record) {
 		long amount = chaguanYushiRecordDboDao.countByConditions(record);
+		List<ChaguanYushiRecordVO> list = new ArrayList<>();
 		List<ChaguanYushiRecordDbo> recordList = chaguanYushiRecordDboDao.findByConditions(page, size, record);
-		ListPage listPage = new ListPage(recordList, page, size, (int) amount);
+		for (ChaguanYushiRecordDbo dbo : recordList) {
+			ChaguanYushiRecordVO vo = new ChaguanYushiRecordVO();
+			vo.setAccountId(dbo.getAccountId());
+			vo.setAccountingAmount(dbo.getAccountingAmount());
+			vo.setAccountingTime(dbo.getAccountingTime());
+			vo.setAgentId(dbo.getAgentId());
+			vo.setBalanceAfter(dbo.getBalanceAfter());
+			vo.setId(dbo.getId());
+			vo.setNickname(agentDboDao.findAgentDboById(dbo.getAgentId()).getNickname());
+			vo.setSummary(dbo.getSummary());
+		}
+		ListPage listPage = new ListPage(list, page, size, (int) amount);
 		return listPage;
 	}
 
---
+++ src/main/java/com/anbang/qipai/admin/web/vo/chaguanvo/ChaguanYushiRecordVO.java
@@ -0,0 +1,85 @@
+package com.anbang.qipai.admin.web.vo.chaguanvo;
+
+import com.dml.accounting.AccountingSummary;
+
+public class ChaguanYushiRecordVO {
+	private String id;
+
+	private String accountId;// 账户id
+
+	private String nickname;// 昵称
+
+	private String agentId;// 馆主id
+
+	private int accountingAmount;// 交易额
+
+	private int balanceAfter;// 余额
+
+	private AccountingSummary summary;// 记账摘要
+
+	private long accountingTime;// 交易时间
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getAgentId() {
+		return agentId;
+	}
+
+	public void setAgentId(String agentId) {
+		this.agentId = agentId;
+	}
+
+	public int getAccountingAmount() {
+		return accountingAmount;
+	}
+
+	public void setAccountingAmount(int accountingAmount) {
+		this.accountingAmount = accountingAmount;
+	}
+
+	public int getBalanceAfter() {
+		return balanceAfter;
+	}
+
+	public void setBalanceAfter(int balanceAfter) {
+		this.balanceAfter = balanceAfter;
+	}
+
+	public AccountingSummary getSummary() {
+		return summary;
+	}
+
+	public void setSummary(AccountingSummary summary) {
+		this.summary = summary;
+	}
+
+	public long getAccountingTime() {
+		return accountingTime;
+	}
+
+	public void setAccountingTime(long accountingTime) {
+		this.accountingTime = accountingTime;
+	}
+}
